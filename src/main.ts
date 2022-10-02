2e74134974b8ddb87a0959a28090e286ea8de543
Thuoq <thuonghamno3xz@gmail.com>
2022-10-02 15:38:39 +0700

    :+1: Impl CORS ORIGIGN

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
