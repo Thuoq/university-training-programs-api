299cc10f377ca349394812cd749206ead2ae0cff
TPNamKhanh <90317223+TPNamKhanh@users.noreply.github.com>
2022-10-24 15:57:25 +0700

    update function in training program content module

--- src/training-program-content/training-program-content.service.ts
+++ src/training-program-content/training-program-content.service.ts
@@ -10,36 +10,36 @@ export class TrainingProgramContentService {
         private readonly prismaService: PrismaService,
         private readonly trainingProgramService: TrainingProgramService,
         private readonly subjectService: SubjectService,
-    ){}
+    ) { }
 
-    async createTrainingProgramContent(payload: CreateTrainingProgramContentDto){
+    async createTrainingProgramContent(payload: CreateTrainingProgramContentDto) {
         const trainingProgram = await this.prismaService.trainingProgramContent.findFirst({
             where: {
                 trainingProgramId: payload.trainingProgramId,
                 subjectId: payload.subjectId,
-                subject:{
+                subject: {
                     id: payload.subjectId
                 },
-                trainingProgram:{
-                    id:payload.trainingProgramId
+                trainingProgram: {
+                    id: payload.trainingProgramId
                 },
             },
         });
 
-        if(trainingProgram){
+        if (trainingProgram) {
             throw new HttpException('Chương trình học đã có môn này', HttpStatus.BAD_REQUEST);
         }
 
-        return this.prismaService.trainingProgramContent.create({data: payload});
+        return this.prismaService.trainingProgramContent.create({ data: payload });
     }
 
-    getListTrainingProgramContent(){
+    getListTrainingProgramContent() {
         return this.prismaService.trainingProgramContent.findMany();
     }
 
-    async getTrainingProgramContent(id: number){
+    async getTrainingProgramContent(id: number) {
         const trainingProgramContent = this.prismaService.trainingProgramContent.findFirst({
-            where:{
+            where: {
                 id: id,
             }
         });
@@ -47,12 +47,21 @@ export class TrainingProgramContentService {
         return trainingProgramContent
     }
 
-    async deleteTrainingProgramContent(id: number){
+    async deleteTrainingProgramContent(id: number) {
         await this.getTrainingProgramContent(id);
         return this.prismaService.trainingProgramContent.delete({
             where: {
                 id,
             }
         });
+    }
+    async updateTrainingProgramContent(id: number, payload: CreateTrainingProgramContentDto) {
+        await this.getTrainingProgramContent(id);
+        return this.prismaService.trainingProgramContent.update({
+            where: {
+                id: id,
+            },
+            data: payload,
+        })
     }
 }
