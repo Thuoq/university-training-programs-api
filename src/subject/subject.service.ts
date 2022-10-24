05d343a1bb84328a310172892e11af202c0c1b4e
Thuoq <thuonghamno3xz@gmail.com>
2022-10-24 21:25:44 +0700

    :+1: subject handling error

--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,24 +1,36 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSubjectDto } from './dtos/createSubject.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { Subject } from '@prisma/client';
+import { PostgresErrorCode } from '../prisma/postgresErrorCodes.enum';
 @Injectable()
 export class SubjectService {
   constructor(private readonly prismaService: PrismaService) {}
   async createSubjectService(payload: CreateSubjectDto): Promise<Subject> {
-    const { prerequisiteSubjectsId, ...body } = payload;
-    if (prerequisiteSubjectsId) {
-      const prerequisiteSubjectsProcessing = payload.prerequisiteSubjectsId.map(
-        (subject) => this.getSubjectById(subject.id),
-      );
-      await Promise.all(prerequisiteSubjectsProcessing);
+    try {
+      const { prerequisiteSubjectsId, ...body } = payload;
+      if (prerequisiteSubjectsId) {
+        const prerequisiteSubjectsProcessing = payload.prerequisiteSubjectsId.map(
+          (subject) => this.getSubjectById(subject.id),
+        );
+        await Promise.all(prerequisiteSubjectsProcessing);
+      }
+      const subject = await this.prismaService.subject.create({
+        data: {
+          ...body,
+          prerequisiteSubjects: { connect: prerequisiteSubjectsId },
+        },
+      });
+      return subject;
+    } catch (error) {
+      if (error?.code === PostgresErrorCode.UniqueViolation) {
+        throw new HttpException(
+          `Duplicate field ${error.meta.target[0]}`,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    return this.prismaService.subject.create({
-      data: {
-        ...body,
-        prerequisiteSubjects: { connect: prerequisiteSubjectsId },
-      },
-    });
   }
   async getSubjectById(id: number): Promise<Subject> {
     const subject = await this.prismaService.subject.findUnique({
