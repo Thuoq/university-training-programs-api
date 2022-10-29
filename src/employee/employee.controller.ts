e4036e83f7772a614d077dfaa8b01b1f11a8ffb0
TPNamKhanh <90317223+TPNamKhanh@users.noreply.github.com>
2022-10-30 01:15:36 +0700

    update function in employee module

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { CreateEmployeeDto } from './dtos/createEmployee.dto';
 import { EmployeeService } from './employee.service';
 import { ApiTags } from '@nestjs/swagger';
@@ -6,7 +6,7 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('Employees')
 @Controller('employees')
 export class EmployeeController {
-  constructor(private readonly employeeService: EmployeeService) {}
+  constructor(private readonly employeeService: EmployeeService) { }
   @Get()
   async getListEmployee() {
     return await this.employeeService.getListEmployee();
@@ -18,5 +18,9 @@ export class EmployeeController {
   @Get(':id')
   async getEmployee(@Param('id') id: number | string) {
     return await this.employeeService.getEmployeeByUnique(id);
+  }
+  @Put(':id')
+  async updateEmployee(@Param('id') id: string, @Body() body: CreateEmployeeDto) {
+    return await this.employeeService.updateEmployee(id, body);
   }
 }
