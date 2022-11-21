6935133f9853ed282e36b08fbad1cad6b2011efd
Thuoq <thuonghamno3xz@gmail.com>
2022-11-21 21:24:00 +0700

    :+1: Add disconected subject

--- src/subject/dtos/createSubject.dto.ts
+++ src/subject/dtos/createSubject.dto.ts
@@ -8,7 +8,7 @@ import {
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-class PrerequisiteSubjectsId {
+export class PrerequisiteSubjectsId {
   id: number;
 }
 export class CreateSubjectDto {
